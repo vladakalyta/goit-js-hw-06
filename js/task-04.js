d2e47e9c73922c12cb1f083572fb3f54b7ab8ea5
vladakalyta <vladakalyta@gmail.com>
2022-08-06 21:51:20 +0300

    task 4
    task 4

---
+++ js/task-04.js
@@ -0,0 +1,15 @@
+const counterValue = document.querySelector('#value');
+const addButton = document.querySelector('button[data-action="increment"]');
+const substructButton = document.querySelector('button[data-action="decrement"]');
+
+let value = 0;
+
+addButton.addEventListener("click", () => {
+  value += 1;
+  counterValue.textContent = value;
+});
+
+substructButton.addEventListener("click", () => {
+  value -= 1;
+  counterValue.textContent = value;
+});
