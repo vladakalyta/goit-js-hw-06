018e471fd5d980b6ba56e38b1be444cd89165561
vladakalyta <vladakalyta@gmail.com>
2022-08-07 16:58:27 +0300

    task 7, 8
    task 7, 8

---
+++ js/task-07.js
@@ -0,0 +1,7 @@
+const fontSizeControl = document.querySelector('#font-size-control');
+const textToChangeSize = document.querySelector('#text');
+
+fontSizeControl.addEventListener('input', () => {
+  let currentFontSize = fontSizeControl.value;
+  textToChangeSize.style.fontSize = `${currentFontSize}px`;
+});
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+ const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+      return alert('All fields must be filled! Please enter your credentials');
+  }
+
+  const userCredentials = {
+    email: email.value,
+    password: password.value
+  }
+  console.log(userCredentials);
+
+  event.currentTarget.reset();
+}
